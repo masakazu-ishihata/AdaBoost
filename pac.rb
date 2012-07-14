c0855da0dec3e3f232d63fa2ebc938053702c3ed
Masakazu Ishihata <masakazu.ishihata@gmail.com>
2012-07-14 16:39:19 +0900

    k-DL learner を追加

--- pac.rb
+++ pac.rb
@@ -3,9 +3,11 @@
 ################################################################################
 # default
 ################################################################################
-@file = "data.txt"
-@n = 10
+@file = "mushroom.txt"
+@n = 5
 @m = 5
+@k = 2
+@@swap = false
 
 ################################################################################
 # Arguments
@@ -17,25 +19,24 @@ OptionParser.new { |opts|
     puts opts
     exit
   }
+  opts.on("-f [string]", "--file", "data file") { |f|
+    @file = f
+  }
   opts.on("-n [int]", "# folds of one cross-validation") { |f|
     @n = f.to_i
   }
   opts.on("-m [int]", "# cross-validations") { |f|
     @m = f.to_i
   }
+  opts.on("-k [int]", "length of decision list ") { |f|
+    @k = f.to_i
+  }
+  opts.on("-s", "--swap", "swap te & tr size"){
+    @@swap = true
+  }
   # parse
   opts.parse!(ARGV)
 }
-
-################################################################################
-# problem generator
-################################################################################
-class MyGenerator
-  #### new ####
-  def initialize(n)
-    @n = n # # variables
-  end
-end
 
 ################################################################################
 # data
@@ -58,6 +59,8 @@ class MyData
       end
       @data.push(datum)
     end
+
+    show
   end
   attr_reader :data
 
@@ -83,12 +86,25 @@ class MyData
     puts "file = #{@file}"
 
     # attributes
+    puts "Attributes"
     puts "attr = #{@vals.size}"
     for i in 0..@vals.size-1
       puts "attr #{i} = #{@vals[i].size} values:  #{@vals[i].keys.join(", ")}"
     end
 
     # data
+    puts "Data"
+    puts "# samples = #{@data.size}"
+
+    # class size
+    c = @vals[0].size
+    num = Array.new(c){|i| 0}
+    @data.each do |datum|
+      num[datum[0]] += 1
+    end
+    for i in 0..c-1
+      puts "class #{i+1} = #{num[i]}"
+    end
     @data[0..10].each do |datum|
       puts "#{datum}"
     end
@@ -184,8 +200,14 @@ class MyTester
 
   #### i-th fold ####
   def fold(ls, i)
-    tr = @sets[i]          # training set
-    te = @data.data - tr   # test set
+    te = @sets[i]          # test set
+    tr = @data.data - te   # training set
+    if @@swap
+      tmp = tr
+      tr = te
+      te = tmp
+    end
+
     fs = [] # averaged f-measures
 
     for i in 0..ls.size-1
@@ -238,8 +260,13 @@ class MyTester
       rec = tp / (tp + fn).to_f
       pre = tp / (tp + fp).to_f
 
+      rec = 0 if tp + fn == 0
+      pre = 0 if tp + fp == 0
+
       # f-measure
       f = 2 * rec * pre / (rec + pre)
+      f = 0 if rec + pre == 0
+
       ave_f += f
     end
     ave_f /= @c.to_f
@@ -292,22 +319,6 @@ class MyDisjLearner < MyLearner
 end
 
 ################################################################################
-# dl learner
-################################################################################
-class MyDLLearner < MyLearner
-  #### new ####
-  def initialize(k)
-    @k = k
-  end
-
-  #### learn ####
-  def learn(data)
-    # 
-    s = Array.new(@c)
-  end
-end
-
-################################################################################
 # Naive Bayes
 ################################################################################
 class MyNaiveBayesLearner < MyLearner
@@ -356,6 +367,143 @@ class MyNaiveBayesLearner < MyLearner
 end
 
 ################################################################################
+# dl learner
+################################################################################
+class MyDLLearner < MyLearner
+  #### new ####
+  def initialize(k)
+    @k = k
+  end
+
+  #### overwrite init ####
+  def init
+    @dl = [] # decision list
+  end
+
+  def show
+    @dl.each do |t|
+      p t
+    end
+  end
+
+  #### overwrite learn ####
+  def learn(data)
+    # divide data by class
+    ss = Array.new(@c){|i| []}
+    data.each do |datum|
+      dc = datum[0]
+      da = datum[1..datum.size-1]
+      ss[dc].push(da)
+    end
+
+    # learn decision list
+    while eval(ss.map{|i| i.size}.join(" + ")) > 0
+      # get a best term for each c
+      best = get_best_term(ss)
+      t = best[0]
+      e = best[1]
+
+      # remove explained samples
+      for c in 0..@c-1
+        ss[c] -= e[c]
+      end
+
+      # add t to decision list
+      m = 0
+      for c in 1..@c-1
+        m = c if e[c].size > e[m].size
+      end
+
+      @dl.push([t, m])
+    end
+  end
+
+  #### get the best term for s ####
+  def get_best_term(ss)
+    # unit clause
+    cs = []  # candidats
+    for i in 0..@n-1
+      for v in 0..@v[i]-1
+        l = [i, v]             # literal
+        e = explained(l, ss)   # samples explained by l
+        cs.push([[l], e])
+      end
+    end
+
+    # find the best
+    best = nil # best
+    while (c = cs.shift) != nil
+      t = c[0]
+      e = c[1]
+      best = c if best == nil || better?(e, best[1])
+      next if t.size == @k
+
+      # enumerate children
+      l = t.last
+      for i in l[0]+1..@n-1
+        for v in 0..@v[i]-1
+          cl = [i, v]
+          ce = explained(cl, e)
+          cs.push([t.clone + [cl], ce]) if eval(ce.map{|i| i.size}.join(" + ")) > 0
+        end
+      end
+    end
+
+    best
+  end
+
+  #### samples explained by a literal l (not a term!) ####
+  def explained_la(l, a)
+    a[ l[0] ] == l[1]
+  end
+  def explained_ta(t, a)
+    t.each do |l|
+      return false if !explained_la(l, a)
+    end
+    return true
+  end
+  def explained(l, ss)
+    e = Array.new(@c){|i| [] }
+
+    for c in 0..@c-1
+      ss[c].each do |da|
+        e[c].push(da) if explained_la(l, da)
+      end
+    end
+
+    e
+  end
+
+  #### e1 is better than e2? ####
+  def better?(e1, e2)
+    ev1 = evaluate(e1)
+    ev2 = evaluate(e2)
+    ev1[0] < ev2[0] || (ev1[0] == ev2[0] && ev1[1] > ev2[1])
+  end
+
+  #### return [min, max] ####
+  def evaluate(e)
+    max = e[0].size
+    for c in 1..@c-1
+      max = e[c].size if max < e[c].size
+    end
+
+    sum = eval( e.map{|i| i.size}.join(" + ") )
+    min = sum - max
+
+    [min,  max]
+  end
+
+  #### overwrite predict ####
+  def predict(da)
+    @dl.each do |t, c|
+      return c if explained_ta(t, da)
+    end
+    return 0 # by definition
+  end
+end
+
+################################################################################
 # main
 ################################################################################
 # learners
@@ -363,6 +511,7 @@ ls = []
 ls.push(MyLearner.new)                   # random
 ls.push(MyDisjLearner.new)               # disj learner
 ls.push(MyNaiveBayesLearner.new)         # naive Bayes
+ls.push(MyDLLearner.new(@k))             # k-DL learner
 
 # repeat n-fold closs validation m times
 t = MyTester.new(@file)
